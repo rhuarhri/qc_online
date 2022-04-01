25664eeefef448267d4b343dffafe3fad3bec15e
Matt Sykes <matt.sykes@3squared.com>
2022-04-01 12:58:58 +0100

    Menu screen and navigation.

--- app/src/main/java/com/three_squared/qc_online/MainActivity.kt
+++ app/src/main/java/com/three_squared/qc_online/MainActivity.kt
@@ -1,33 +1,63 @@
 package com.three_squared.qc_online
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    var drawerLayout: DrawerLayout? = null
-    var actionBarDrawerToggle: ActionBarDrawerToggle? = null
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawerLayout = findViewById(R.id.my_drawer_layout)
-        actionBarDrawerToggle =
-            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        var drawerLayout: DrawerLayout? = null
+        var actionBarDrawerToggle: ActionBarDrawerToggle? = null
+        val navView: NavigationView = findViewById(R.id.nav_view)
 
-        drawerLayout?.addDrawerListener(actionBarDrawerToggle!!)
-        actionBarDrawerToggle!!.syncState()
+        appBarConfiguration = AppBarConfiguration(
+            navController.graph, drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (actionBarDrawerToggle!!.onOptionsItemSelected(item)) {
-            true
-        } else super.onOptionsItemSelected(item)
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.navigation_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        println("item pressed was ${item.title}")
+        return super.onOptionsItemSelected(item)
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return if (actionBarDrawerToggle!!.onOptionsItemSelected(item)) {
+//            true
+//        } else super.onOptionsItemSelected(item)
+//    }
 }
